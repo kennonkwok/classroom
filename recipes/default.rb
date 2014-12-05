4b9f6fb53fd62baabb14afa1c940c39061b7c278
Kennon Kwok <kennon@getchef.com>
2014-12-04 21:33:20 -0800

    clean up attributes

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 # include_recipe 'classroom::nginx'
-node.set['build_essential']['compiletime'] = true
+node.set['build_essential']['compile_time'] = true
 include_recipe 'build-essential'
 include_recipe 'docker'
 include_recipe 'classroom::consul'
